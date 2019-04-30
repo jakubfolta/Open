ae9f6c711abd421eea3d01ac0987c4669ec549c0
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-30 22:23:57 +0100

    solidifying knowledge

--- open2.py
+++ open2.py
@@ -12,5 +12,10 @@ import webbrowser
 result = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 result.rise_for_status()
 
-# TODO: Parse this site and retrieve top search results with bs4 module.
+# Parse this site and retrieve top search results with bs4 module.
+result_text = bs4.BeautifulSoup(result.text)
+link_elements = result_text.select('.r a')
+
 # TODO: Open new tabs with top links using webbrowser module.
+tabs_opened = min(3, len(link_elements))
+for i in range
