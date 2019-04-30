f079a9d2fb1d8673349c01fa3ddf3958fef01b3b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-30 22:17:40 +0100

    solidifying knowledge

--- open2.py
+++ open2.py
@@ -2,12 +2,15 @@
 
 """open2.py - Opens several Google search results."""
 
-# TODO: Import essential modules.
+# Import essential modules.
 import sys
 import requests
 import bs4
 import webbrowser
 
-# TODO: Download the google page with entered search words with requests module.
+# Download the google page with entered search words with requests module.
+result = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+result.rise_for_status()
+
 # TODO: Parse this site and retrieve top search results with bs4 module.
 # TODO: Open new tabs with top links using webbrowser module.
