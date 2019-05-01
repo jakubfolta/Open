cdc5c158bf08ee78a14e2fa2c4d8c04461ad6fc6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-01 13:52:30 +0100

    solidifying knowledge

--- open4.py
+++ open4.py
@@ -1,6 +1,11 @@
 #! python3
 
-# TODO: Import essential modules.
+# Import essential modules.
+import sys
+import requests
+import bs4
+import webbrowser
+
 # TODO: Download google search website with searched words from command line with requests module.
 # TODO: Parse this website and retrieve link elements with bs4 module.
 # TODO: Set variable with numbers of tabs to open.
