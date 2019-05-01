1d4f4e06c8d6b428bda7773325f3bc039bb0f217
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-01 07:29:20 +0100

    solidifying knowledge

--- open3.py
+++ open3.py
@@ -2,12 +2,15 @@
 
 """open3.py - Opens several Google search results."""
 
-# TODO: Import essential modules.
+# Import essential modules.
 import sys
 import requests
 import bs4
 import webbrowser
 
 # TODO: Download the google search page with words entered in command line with requests module.
+result = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+result.raise_for_status()
+
 # TODO: Parse this site and retrieve top search results using bs4 module.
 # TODO: Open every link in new tab using webbrowser.
