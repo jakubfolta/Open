17ae4e9cf322c1284276ec4076b4fc8e03598c0f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-01 14:10:57 +0100

    solidifying knowledge

--- open4.py
+++ open4.py
@@ -6,7 +6,17 @@ import requests
 import bs4
 import webbrowser
 
-# TODO: Download google search website with searched words from command line with requests module.
-# TODO: Parse this website and retrieve link elements with bs4 module.
-# TODO: Set variable with numbers of tabs to open.
-# TODO: Open every link in new tab with webbrowser module.
+# Download google search website with searched words from command line with requests module.
+result = requests.get('https://www.google.com/search?client=' + ' '.join(sys.argv[1:]))
+result.raise_for_status()
+
+# Parse this website and retrieve link elements with bs4 module.
+result_text = bs4.BeautifulSoup(result.text)
+link_elements = result_text.select('.r a')
+
+# Set variable with numbers of tabs to open.
+tabs_number = min(3, len(link_elements))
+
+# Open every link in new tab with webbrowser module.
+for i in range(tabs_number):
+    webbrowser.open('http://google.com' + link_elements[i].get('href'))
