bd09bffd699eb0d1bc2a9e9120c19c4d6a5611b6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-01 07:30:03 +0100

    solidifying knowledge

--- open3.py
+++ open3.py
@@ -10,7 +10,7 @@ import webbrowser
 
 # TODO: Download the google search page with words entered in command line with requests module.
 result = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
-result.raise_for_status()
+result.raise_for_status(fds)
 
 # TODO: Parse this site and retrieve top search results using bs4 module.
 # TODO: Open every link in new tab using webbrowser.
