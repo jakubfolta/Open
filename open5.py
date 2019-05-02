de042551b785218d09ca72d78ee65f1bf03650d6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-02 09:20:36 +0100

    solidifying knowledge

---
+++ open5.py
@@ -0,0 +1,14 @@
+#! python3
+
+"""open5.py - Opens several Google search results."""
+
+# Import essential modules.
+import sys
+import requests
+import bs4
+import webbrowser
+
+# TODO: Download google search site with searched words from command line using requests module.
+# TODO: Parse this site and retrieve link elements using bs4 module.
+# TODO: Set variable with tabs number to be opened.
+# TODO: Open every link in new tab.
